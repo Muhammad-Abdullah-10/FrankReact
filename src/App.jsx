66e5a3278c27c56b5717a130cfee44eb0e63ad8f
Muhammad-Abdullah-10 <59797973+Muhammad-Abdullah-10@users.noreply.github.com>
2025-09-25 19:40:18 +0500

    Logo Slider Component Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,11 @@
 import React from 'react'
 import './App.css'
 import './custom.css'
-import arabianBusiness from "./assets/img/news-logo/arabianBusiness.svg";
-import forbes from "./assets/img/news-logo/forbesMiddleEast.svg";
-import gulfNews from "./assets/img/news-logo/gulfNews.svg";
-import khaleejTimes from "./assets/img/news-logo/khaleejTimes.svg";
-import theNational from "./assets/img/news-logo/theNational.svg";
-import zawya from "./assets/img/news-logo/zawya.svg";
-import dubaiOne from "./assets/img/news-logo/dubaiOne.svg";
+import LogoSlider from './components/Sliders/LogoSlider'
+
 
 function App() {
-  const logos = [
-    { src: arabianBusiness, alt: "Arabian Business" },
-    { src: forbes, alt: "Forbes Middle East" },
-    { src: gulfNews, alt: "Gulf News" },
-    { src: khaleejTimes, alt: "Khaleej Times" },
-    { src: theNational, alt: "The National" },
-    { src: arabianBusiness, alt: "Arabian Business" },
-    { src: forbes, alt: "Forbes Middle East" },
-    { src: zawya, alt: "Zawya" },
-    { src: dubaiOne, alt: "Dubai One" },
-    { src: theNational, alt: "The National" },
-  ];
+  
   return (
     <>
        <section className="wrapper-form-banner mt-[-15%] md:mt-[-8%] lg:mt-[-8%] bg-white w-full h-auto ">
@@ -57,15 +41,7 @@ function App() {
 
         </div>
       </section>
-      <div className="wrapper-logo-scroll flex flex-row h-auto w-full my-[25px] lg:my-[75px]">
-        <div className="scroll" style={{ "--t": "15s" }}>
-              <div>
-                {logos.map((logo, i) => (
-                  <img key={i} src={logo.src} alt={logo.alt} />
-                ))}
-              </div>
-        </div>
-      </div>
+      <LogoSlider />
     </>
     
   )
---
+++ src/components/Sliders/LogoSlider.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import arabianBusiness from "./assets/img/news-logo/arabianBusiness.svg";
+import forbes from "./assets/img/news-logo/forbesMiddleEast.svg";
+import gulfNews from "./assets/img/news-logo/gulfNews.svg";
+import khaleejTimes from "./assets/img/news-logo/khaleejTimes.svg";
+import theNational from "./assets/img/news-logo/theNational.svg";
+import zawya from "./assets/img/news-logo/zawya.svg";
+import dubaiOne from "./assets/img/news-logo/dubaiOne.svg";
+
+function LogoSlider() {
+    const logos = [
+        { src: arabianBusiness, alt: "Arabian Business" },
+        { src: forbes, alt: "Forbes Middle East" },
+        { src: gulfNews, alt: "Gulf News" },
+        { src: khaleejTimes, alt: "Khaleej Times" },
+        { src: theNational, alt: "The National" },
+        { src: arabianBusiness, alt: "Arabian Business" },
+        { src: forbes, alt: "Forbes Middle East" },
+        { src: zawya, alt: "Zawya" },
+        { src: dubaiOne, alt: "Dubai One" },
+        { src: theNational, alt: "The National" },
+    ];
+  return (
+    <div className="wrapper-logo-scroll flex flex-row h-auto w-full my-[25px] lg:my-[75px]">
+        <div className="scroll" style={{ "--t": "15s" }}>
+              <div>
+                {logos.map((logo, i) => (
+                  <img key={i} src={logo.src} alt={logo.alt} />
+                ))}
+              </div>
+        </div>
+      </div>
+  )
+}
+
+export default LogoSlider
