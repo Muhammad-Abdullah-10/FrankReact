510b13438e4e73c71a5f9fb20ae1bb57a4af970c
Muhammad-Abdullah-10 <59797973+Muhammad-Abdullah-10@users.noreply.github.com>
2025-09-27 03:42:57 +0500

    Hero Slider Working as compontent

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.css'
 import './custom.css'
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import 'swiper/css';
 import LogoSlider from './components/Sliders/LogoSlider'
 import HeroForm from './components/Hero/HeroForm'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer';
 import LeftImageRightContent from './components/ImagePlusContent/LeftContentRightImage';
 import RightContentLeftImage from './components/ImagePlusContent/RightContentLeftImage';
+import HeroSlider from './components/Hero/HeroSlider';
 
 
 
@@ -16,6 +18,7 @@ function App() {
   return (
     <>
       <Header />
+      <HeroSlider />
       <HeroForm />
       <LogoSlider />
       <LeftImageRightContent />
---
+++ src/components/Hero/HeroSlider.jsx
@@ -0,0 +1,106 @@
+import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import {  Pagination, } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+function HeroSlider() {
+  return (
+    <div className="wrapper-banner-home">
+       
+      <div className="swiper homeBanner">
+        <div className="swiper-wrapper">
+        <Swiper 
+        modules={[Pagination]}
+        slidesPerView={1}
+        
+        >
+          <SwiperSlide className="swiper-slide">
+            <div className="wrapper-slide" style={{background: "url('../src/assets/img/FrankPorter_main_baner_1.jpeg')", backgroundSize: "contain"}}>
+              <div className="slide-data">
+                  <h2 className="heading-wrapper font-futuraNormal">Airbnb
+                    Management Dubai</h2>
+                    <h3 className="desc-wrapper">
+                      Premier property management for short-term rentals across Dubai
+                    </h3>
+                    <div className="wrapper-button flex flex-col sm:flex-row  max-w-[100%] md:max-w-[80%] md:mx-auto lg:mx-0  lg:max-w-[50%] md:mt-[20px]">
+                      <div className="flex flex-row justify-start items-center w-[80%] mx-auto  sm:w-[100%]  my-[10px]">
+                        <a href="#" className="bg-blackButton w-[100%] h-auto font-futuraLight text-white font-bold text-[16px] py-[10px] rounded-[5px]
+                        text-center uppercase border-[#2D2D2C] border-[2px] border-solid hover:text-[#2D2D2C] hover:bg-white  hover:font-[600]">
+                          Manage My Property
+                        </a>
+                      </div>
+                      <div className="flex flex-row justify-start items-center w-[80%] mx-auto pl-[0px]  sm:w-[100%] sm:pl-[20px] my-[10px]">
+                        <a href="#" className="bg-white w-[100%] h-auto font-futuraLight text-[#2d2d2d2] font-bold text-[16px] py-[10px] rounded-[5px]
+                        text-center uppercase border-[#2D2D2C] border-[2px] border-solid hover:text-white hover:bg-blackButton hover:font-[600] ">
+                          Book Now
+                        </a>
+                      </div>
+                    </div>
+              </div>
+            </div>
+          </SwiperSlide>
+          
+          
+          <SwiperSlide className="swiper-slide">
+            <div className="wrapper-slide" style={{background: "url('../src/assets/img/FrankPorter_main_baner_2.jpeg')", backgroundSize: "contain"}}>
+              <div className="slide-data">
+                  <h2 className="heading-wrapper font-futuraNormal">Airbnb
+                    Management Dubai</h2>
+                    <h3 className="desc-wrapper">
+                      Premier property management for short-term rentals across Dubai
+                    </h3>
+                    <div className="wrapper-button flex flex-col sm:flex-row  max-w-[100%] md:max-w-[80%] md:mx-auto lg:mx-0  lg:max-w-[50%] md:mt-[20px]">
+                      <div className="flex flex-row justify-start items-center w-[80%] mx-auto  sm:w-[100%]  my-[10px]">
+                        <a href="#" className="bg-blackButton w-[100%] h-auto font-futuraLight text-white font-bold text-[16px] py-[10px] rounded-[5px]
+                        text-center uppercase border-[#2D2D2C] border-[2px] border-solid hover:text-[#2D2D2C] hover:bg-white  hover:font-[600]">
+                          Manage My Property
+                        </a>
+                      </div>
+                      <div className="flex flex-row justify-start items-center w-[80%] mx-auto pl-[0px]  sm:w-[100%] sm:pl-[20px] my-[10px]">
+                        <a href="#" className="bg-white w-[100%] h-auto font-futuraLight text-[#2d2d2d2] font-bold text-[16px] py-[10px] rounded-[5px]
+                        text-center uppercase border-[#2D2D2C] border-[2px] border-solid hover:text-white hover:bg-blackButton hover:font-[600] ">
+                          Book Now
+                        </a>
+                      </div>
+                    </div>
+              </div>
+            </div>
+          </SwiperSlide>
+          
+          
+          <SwiperSlide className="swiper-slide">
+            <div className="wrapper-slide" style={{background: "url('../src/assets/img/FrankPorter_main_baner_3.jpeg')", backgroundSize: "contain"}}>
+              <div className="slide-data">
+                  <h2 className="heading-wrapper font-futuraNormal">Airbnb
+                    Management Dubai</h2>
+                    <h3 className="desc-wrapper">
+                      Premier property management for short-term rentals across Dubai
+                    </h3>
+                    <div className="wrapper-button flex flex-col sm:flex-row  max-w-[100%] md:max-w-[80%] md:mx-auto lg:mx-0  lg:max-w-[50%] md:mt-[20px]">
+                      <div className="flex flex-row justify-start items-center w-[80%] mx-auto  sm:w-[100%]  my-[10px]">
+                        <a href="#" className="bg-blackButton w-[100%] h-auto font-futuraLight text-white font-bold text-[16px] py-[10px] rounded-[5px]
+                        text-center uppercase border-[#2D2D2C] border-[2px] border-solid hover:text-[#2D2D2C] hover:bg-white  hover:font-[600]">
+                          Manage My Property
+                        </a>
+                      </div>
+                      <div className="flex flex-row justify-start items-center w-[80%] mx-auto pl-[0px]  sm:w-[100%] sm:pl-[20px] my-[10px]">
+                        <a href="#" className="bg-white w-[100%] h-auto font-futuraLight text-[#2d2d2d2] font-bold text-[16px] py-[10px] rounded-[5px]
+                        text-center uppercase border-[#2D2D2C] border-[2px] border-solid hover:text-white hover:bg-blackButton hover:font-[600] ">
+                          Book Now
+                        </a>
+                      </div>
+                    </div>
+              </div>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+        </div>
+        <div className="swiper-pagination"></div>
+      </div>
+    </div>
+
+  )
+}
+
+export default HeroSlider
