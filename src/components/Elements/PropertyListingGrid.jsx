8755e023a7230b6cc79acf4e58cb3d1dcd15b48d
Muhammad-Abdullah-10 <59797973+Muhammad-Abdullah-10@users.noreply.github.com>
2025-10-11 04:24:27 +0500

    Context API data coming and can be accessed in the components

--- src/components/Elements/PropertyListingGrid.jsx
+++ src/components/Elements/PropertyListingGrid.jsx
@@ -1,30 +1,27 @@
-import React , {useContext} from 'react'
-import PropertyCard from './PropertyCard'
-import PropertyContext from '../../context/PropertyContext';
+import React, { useContext, useState, useEffect } from "react";
+import PropertyCard from "./PropertyCard";
+import PropertyContext from "../../context/PropertyContext";
 
 function PropertyListingGrid() {
-    const data = useContext(PropertyContext)
+  const propertyData = useContext(PropertyContext);
+  const [propertyList, setPropertyList] = useState([]);
+
+  useEffect(() => {
+    if (Array.isArray(propertyData) && propertyData.length > 0) {
+      setPropertyList(propertyData);
+    }
+  }, [propertyData]);
+
+  console.log("propertyList:", propertyList);
   return (
-      <div className="wrapper-property-cards">
-        <div className="grid grid-cols-1 md:grid-cols-2 xl:px-5 xl:grid-cols-3 3xl:grid-cols-3 2xl:max-w-3/4 2xl:mx-auto gap-8">
-          {/* <!-- property-card --> */}
-           <PropertyCard />
-           <PropertyCard />
-           <PropertyCard />
-           <PropertyCard />
-           <PropertyCard />
-           <PropertyCard />
-           <PropertyCard />
-           <PropertyCard />
-           <PropertyCard />
-           <PropertyCard />
-           <PropertyCard />
-           <PropertyCard />
-          {/* <!-- property-card --> */}
-     
-        </div>
+    <div className="wrapper-property-cards">
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:px-5 xl:grid-cols-3 3xl:grid-cols-3 2xl:max-w-3/4 2xl:mx-auto gap-8">
+        {propertyList.map((item) => (
+          <PropertyCard key={item.id} property={item} />
+        ))}
       </div>
-  )
+    </div>
+  );
 }
 
-export default PropertyListingGrid
+export default PropertyListingGrid;
